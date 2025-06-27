0c06c3dcd300c0955a56200d32ec3ca502f7fefc
CallMeVinn <vinndeveloper@gmail.com>
2025-06-28 01:16:07 +0700

    update database.js

--- handler/database.js
+++ handler/database.js
@@ -1,6 +1,11 @@
+const { QuickMongo } = require("quick-mongo-super");
+
 module.exports = async(client) => {
     client.db.on("connect", async() => {
+        client.db = new QuickMongo(client.db, { name: "Clash" });
+        
         const ping = await client.db.ping();
+        
         console.log("[Database] Connected. ✅", `Read: ${ping.readLatency}ms | Write: ${ping.writeLatency}ms | Delete: ${ping.deleteLatency}ms`);
     });
     client.db.on("disconnect", () => console.log("[Database] Disconnected. ⚠️"));
